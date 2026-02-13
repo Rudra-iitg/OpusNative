4fc0cee990668a6683bfd2d95b338a8f3637e02c
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-02-14 02:12:34 +0530

    Initial Commit

---
+++ OpusNative/OpusNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpusNativeApp.swift
+//  OpusNative
+//
+//  Created by RUDRA JHA on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct OpusNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
